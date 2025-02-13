90731cd8d3c5a78221e807cf1f0d3c9a70eab9cf
kuldeep1203 <kuldeeppawar427@gmail.com>
2025-02-13 10:42:17 +0530

    first time

---
+++ src/main/java/first/time/runner2/Runner2Application.java
@@ -0,0 +1,13 @@
+package first.time.runner2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Runner2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Runner2Application.class, args);
+	}
+
+}
---
+++ src/test/java/first/time/runner2/Runner2ApplicationTests.java
@@ -0,0 +1,13 @@
+package first.time.runner2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Runner2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
